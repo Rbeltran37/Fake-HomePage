d017dc598a2cbec9718e96ad5a7239bce3b8b1a0
Roberto Beltran <rbeltran37@gmail.com>
2022-12-13 01:59:16 -0500

    upload 5

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='./Fake-HomePage/images/img-9.jpg'
+              src='Fake-HomePage/images/img-9.jpg'
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src='./Fake-HomePage/images/img-2.jpg'
+              src='https://rbeltran37.github.io/Fake-HomePage/images/img-2.jpg'
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
