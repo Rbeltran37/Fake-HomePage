09153ce8d05a4993b6155c909c2582f3d3e341b6
Roberto Beltran <rbeltran37@gmail.com>
2022-12-13 01:32:45 -0500

    upload 2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="Fake-HomePage/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/products" element={<Products />} />
         <Route path="/sign-up" element={<SignUp />} />
